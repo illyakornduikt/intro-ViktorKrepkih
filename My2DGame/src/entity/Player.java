44bed0dbd24186412d6cde4c19d6d65cb6e73df8
Viktor Krepkih <viktor.krepkih@gmail.com>
2024-11-06 19:17:45 +0200

    2024.11.06

--- My2DGame/src/entity/Player.java
+++ My2DGame/src/entity/Player.java
@@ -14,7 +14,7 @@ public class Player extends Entity{
     KeyHandler keyH;
     public final int screenX;
     public final int screenY;
-    int hasKey = 0;
+    public int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
@@ -119,16 +119,33 @@ public class Player extends Entity{
 
             switch(objectName) {
             case "Key":
+                gp.playSE(1);
                 hasKey++;
                 gp.obj[i] = null;
-                System.out.println("Key:"+hasKey);
+                gp.ui.showMessage("You got a key!");
                 break;
             case "Door":
                 if(hasKey > 0)	{
+                        gp.playSE(3);
                         gp.obj[i] = null;
                         hasKey--;
+                        gp.ui.showMessage("You opened the door!");
                     }
+                else {
+                    gp.ui.showMessage("You need a key!");
+                }
                     System.out.println("Key:"+hasKey);
+                    break;
+                case "Boots":
+                    gp.playSE(2);
+                    speed += 2;
+                    gp.obj[i] = null;
+                    gp.ui.showMessage("Speed up!");
+                    break;
+                case "Chest":
+                    gp.ui.gameFinished = true;
+                    gp.stopMusic();
+                    gp.playSE(4);
                     break;
             }
         }
